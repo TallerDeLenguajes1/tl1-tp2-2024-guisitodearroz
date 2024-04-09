c854e5641a7b6f405410ee5b2fde665bee7c9e27
Diego Cortez <dc936572@gmail.com>
2024-04-09 15:28:23 -0300

    punto puntero indexado

---
+++ tp2_1_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+     #define N 20
+    int i;
+    double vt[N],*pV;
+    pV=vt;
+    for(i = 0;i<N; i++)
+    {
+        *(pV+i)=1+rand()%100;
+        //printf("%f ", vt[i]);
+        printf("%f " ,*(pV+i));
+        }   
+    return 0;
+}
