3c3b541730aeecb8c1bdd900bc66ffc9357a316b
guisitodearroz <dc936572@gmail.com>
2024-04-14 18:17:37 -0300

    punto 4 a

--- tp2_4.c
+++ tp2_4.c
@@ -1,25 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-char tipo[6]={"intel","AMD","Celeron"."Athlon" , "Core","Pentium"};
+#include <string.h>
+#include <time.h>
+
+char tipo[6][10] = {"intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
 
 struct cpu
 {
-  int velocidad;
-  int anio;
-  int cantidad;
-  char *tipo_cpu;  
-}typedef Cpu;
+    int velocidad;
+    int anio;
+    int cantidad;
+    char *tipo_cpu;
+};
+
+// Función para crear valores aleatorios
+int valAleatorios(int min, int max)
+{
+    return min + rand() % (max - min + 1);
+}
+
+// Función para crear una PC
+struct cpu crearCompu()
+{
+    struct cpu pc;
+    pc.velocidad = valAleatorios(1, 3);
+    pc.anio = valAleatorios(2015, 2023);
+    pc.cantidad = valAleatorios(1, 8);
+
+    // Asignar el tipo de procesador aleatorio
+    int indiceTipo = rand() % 6;
+    pc.tipo_cpu = malloc(strlen(tipo[indiceTipo]) + 1);
+    strcpy(pc.tipo_cpu, tipo[indiceTipo]);
+
+    return pc;
+}
 
 int main(int argc, char const *argv[])
 {
-    int i;
-    Cpu nuevoCpu[5];
-    for ( i = 0; i =< 5; i++)
-    {
-        nuevoCpu->velocidad= 
-        nuevoCpu->tipo_cpu[i]=tipo[i];
-        
-    }
-    
+    srand(time(NULL));
+
+    struct cpu miPc = crearCompu();
+    printf("Velocidad: %d GHz\n", miPc.velocidad);
+    printf("El año de fabricacion: %d\n", miPc.anio);
+    printf("Cantidad de nucleos: %d\n", miPc.cantidad);
+    printf("Tipo de procesador %s\n", miPc.tipo_cpu);
+
+    // Liberar la memoria asignada dinámicamente
+    free(miPc.tipo_cpu);
+
     return 0;
 }
